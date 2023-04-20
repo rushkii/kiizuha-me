b9fd27d035177d2e77dc21488cec64b666c7dc11
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-04-20 22:11:59 +0700

    header: change styles for the `<Header/>` component
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,14 +3,9 @@ import Link from "next/link";
 
 const Headers = () => {
   return (
-    <nav className="w-full backdrop-blur border-b-[0.1px] relative z-10 border-b-[#cfcfd6] dark:border-[#cbd5e129]
-                    supports-backdrop-blur:bg-white/60 sm:text-white lg:text-black dark:lg:text-white font-rubik">
-      <div className="px-8 flex justify-between">
-        <Link href="/" title="Abyss" className="h-12 flex items-center sm:text-lg 2xl:text-2xl font-bold bg-gradient-to-r
-                                              from-blue-600 to-sky-500 dark:from-blue-500 dark:to-purple-400
-                                              from-25% to-75%"
-          style={{backgroundClip: "text", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent"}}
-        >
+    <nav className="w-full backdrop-blur border-b-[0.1px] relative z-10 border-b-[#cfcfd6] dark:border-[#cbd5e129] supports-backdrop-blur:bg-white/60 font-rubik">
+      <div className="px-8 flex sm:justify-center lg:justify-between">
+        <Link href="/" title="Abyss" className="h-12 flex items-center sm:text-xl 2lg:text-2xl font-bold bg-gradient-to-r from-blue-600 to-sky-500 dark:from-blue-500 dark:to-purple-400 from-25% to-75%" style={{backgroundClip: "text", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent"}}>
           kiizuha.dev
         </Link>
       </div>
