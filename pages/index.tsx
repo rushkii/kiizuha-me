7c48fc6ee2b3c8ba8cc1f18cbfb0413eb33fe84d
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-04-23 22:44:46 +0700

    fix: use HTML escape from `'` to `&#39;`
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home(): JSX.Element {
           </div>
           <div className="flex flex-col sm:items-center sm:justify-center 2lg:items-start 2lg:justify-start sm:ml-0 2lg:ml-10 xl:ml-10">
             <div className="font-semibold text-xl">Hello</div>
-            <div className="font-extrabold sm:text-3xl md:text-4xl xl:text-6xl">I'm Kiizuha</div>
+            <div className="font-extrabold sm:text-3xl md:text-4xl xl:text-6xl">I&#39;m Kiizuha</div>
             <div className="font-bold sm:text-sm md:text-base xl:text-xl">A Junior Developer</div>
             <div className="mt-4 flex-wrap sm:w-[20rem] md:w-[30rem] text-slate-700 dark:text-slate-300 sm:ml-10 2lg:ml-0">
               I am a full-stack developer, and I enjoy creating self-projects when I have the inspiration and motivation to do so.
@@ -24,9 +24,9 @@ export default function Home(): JSX.Element {
             About Me
           </div>
           <div className="sm:text-base xl:text-lg text-slate-700 dark:text-slate-300">
-            Hi, I'm Muhammad Rizki, and my alias name is Kiizuha Kanazawa. I'm a junior developer with 5 years of experience in the Python programming language and am currently self-learning TypeScript.
+            Hi, I&#39;m Muhammad Rizki, and my alias name is Kiizuha Kanazawa. I&#39;m a junior developer with 5 years of experience in the Python programming language and am currently self-learning TypeScript.
             <br/><br/>
-            I have developed many bots for Telegram and Discord, REST APIs using FastAPI, scraped websites, developed front-ends using NextJS, ReactJS, React Native, Flutter/Dart, Kotlin, and built back-ends, especially REST APIs, using Python's FastAPI and Flask.
+            I have developed many bots for Telegram and Discord, REST APIs using FastAPI, scraped websites, developed front-ends using NextJS, ReactJS, React Native, Flutter/Dart, Kotlin, and built back-ends, especially REST APIs, using Python&#39;s FastAPI and Flask.
             <br/><br/>
             I have used MongoDB and MySQL for databases, and I often use MongoDB for my own Telegram and Discord bots because it requires fast response times. MongoDB is a NoSQL database, so it is lightweight and faster.
           </div>
