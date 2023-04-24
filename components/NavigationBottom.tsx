7a2542d875bd1b6a00ae43d74f99955c8f169fb7
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-04-25 03:59:13 +0700

    nav: change `w-[3rem]` to `px-1`
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- components/NavigationBottom.tsx
+++ components/NavigationBottom.tsx
@@ -13,7 +13,7 @@ const NavigationBottom = () => {
     <div className="sticky lg:hidden bottom-3 w-full flex justify-center items-center mb-3">
       <div className="flex flex-row py-2 px-2 lg:py-3 lg:px-3 bg-slate-500 border border-[#ffffff22] backdrop-blur-xl rounded-xl space-x-2 lg:space-x-3 bg-opacity-30">
         <Link href="/">
-          <div className="flex flex-col w-[3rem] items-center justify-center text-center space-y-2">
+          <div className="flex flex-col px-1 items-center justify-center text-center space-y-2">
             {currentPath === "/" ?
               <BsFillPersonFill
                 className="grid place-items-center text-[20px] lg:text-[30px] sm:text-[25px] text-black dark:text-white"
@@ -26,7 +26,7 @@ const NavigationBottom = () => {
           </div>
         </Link>
         <Link href="/projects">
-          <div className="flex flex-col w-[3rem] overflow-hidden items-center justify-center text-center space-y-2">
+          <div className="flex flex-col px-1 overflow-hidden items-center justify-center text-center space-y-2">
             {currentPath === "/projects" ?
               <AiFillProject
                 className="grid place-items-center text-[20px] lg:text-[30px] sm:text-[25px] text-black dark:text-white"
