3fe68cc963a34d0426d30dd0284b63e4e8fa6625
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-04-24 16:21:32 +0700

    pages: customize the `<Head/>` element in the main app
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,18 @@ import { fontRubik } from '@/data/fonts'
 import type { AppProps } from 'next/app'
 import Headers from '@/components/Header';
 import Footer from '@/components/Footer';
+import Head from 'next/head';
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Kiizuha Kanazawa</title>
+        <meta name="description" content="Kiizuha Kanazawa's personal website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <style jsx global>{`
         :root {
           --rubik-font: ${fontRubik.style.fontFamily};
