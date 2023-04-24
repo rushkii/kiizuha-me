f0b3fae03590a8b3f87d49319f2f00d90db7dcc2
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-04-24 17:37:21 +0700

    components: update syles and add sub-title navigation in the `<NavigationBottom/>`
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- components/NavigationBottom.tsx
+++ components/NavigationBottom.tsx
@@ -5,16 +5,22 @@ import { BiGitRepoForked } from "react-icons/bi";
 const NavigationBottom = () => {
   return (
     <div className="sticky lg:hidden bottom-3 w-full flex justify-center items-center mb-3">
-      <div className="flex flex-row py-2 px-2 lg:py-3 lg:px-3 sm:py-3 sm:px-2 bg-slate-500 border border-[#ffffff22] backdrop-blur-xl rounded-xl space-x-2 sm:space-x-4 lg:space-x-5 bg-opacity-30">
+      <div className="flex flex-row py-2 px-2 lg:py-3 lg:px-3 bg-slate-500 border border-[#ffffff22] backdrop-blur-xl rounded-xl space-x-2 lg:space-x-3 bg-opacity-30">
         <Link href="/">
-          <BsFillPersonFill
-            className="grid place-items-center text-[20px] lg:text-[30px] sm:text-[25px] text-black dark:text-white"
-          />
+          <div className="flex flex-col w-[3rem] bg-gren-300 items-center justify-center text-center space-y-2">
+            <BsFillPersonFill
+              className="grid place-items-center text-[20px] lg:text-[30px] sm:text-[25px] text-black dark:text-white"
+            />
+            <div className="text-xs line-clamp-1">Profile</div>
+          </div>
         </Link>
         <Link href="/projects">
-          <BiGitRepoForked
-            className="grid place-items-center text-[20px] lg:text-[30px] sm:text-[25px] text-black dark:text-white"
-          />
+          <div className="flex flex-col w-[3rem] overflow-hidden bg-gren-300 items-center justify-center text-center space-y-2">
+            <BiGitRepoForked
+              className="grid place-items-center text-[20px] lg:text-[30px] sm:text-[25px] text-black dark:text-white"
+            />
+            <div className="text-xs line-clamp-1">Projects</div>
+          </div>
         </Link >
       </div>
     </div>
