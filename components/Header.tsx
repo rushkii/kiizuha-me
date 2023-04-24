4e867c9c418fd0f50e49aa414342d983d90a536e
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-04-24 16:15:14 +0700

    components: update styles for the `<Header/>` component
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,13 +8,13 @@ const Headers = () => {
   let currentPath = route.pathname;
 
   return (
-    <nav className="w-full backdrop-blur border-b-[0.1px] relative z-10 border-b-[#cfcfd6] dark:border-[#cbd5e129] supports-backdrop-blur:bg-white/60 font-rubik">
+    <nav className="overflow-x-hidden w-full backdrop-blur border-b-[0.1px] relative z-10 border-b-[#cfcfd6] dark:border-[#cbd5e129] supports-backdrop-blur:bg-white/60 font-rubik">
       <div className="px-8 flex sm:justify-center lg:justify-between">
         <Link href="/" title="Abyss" className="h-12 flex items-center sm:text-xl 2lg:text-2xl font-bold bg-gradient-to-r from-blue-600 to-sky-500 dark:from-blue-500 dark:to-purple-400 from-25% to-75%" style={{backgroundClip: "text", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent"}}>
           kiizuha.dev
         </Link>
 
-        <ul className="flex space-x-5 border-blue-600">
+        <ul className="2lg:flex space-x-5 border-blue-600 hidden">
           <li className="h-auto">
             <Link
               href="/projects"
